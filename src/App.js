3341f361b3271206acd0854397412e8885936134
DanTGL <33489389+DanTGL@users.noreply.github.com>
2021-05-19 15:52:09 +0200

    Added window stacking

--- src/App.js
+++ src/App.js
@@ -2,20 +2,51 @@ import React, { Component} from "react";
 import "./App.css";
 import Window from "./Window.js"
 
-class App extends Component{
-  onClick() {
-    console.log("test");
+const apps = [
+  {
+    color: "blue"
+  },
+  {
+    color: "green"
+  },
+  {
+    color: "yellow"
   }
+];
 
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-        <Window onTouch={this.onClick} color="blue" />
-        <Window onClick={this.onClick} color="green" />
-      </div>
-    );
-  }
+function App() {
+  const rndManagerRef = React.useRef({
+    maxZIndex: 0,
+    prevDraggedNode: null
+  });
+
+
+  const openApp = (id) => {
+    current_index = window_order.indexOf(id);
+
+    if (current_index != -1) {
+      window_order.splice(current_index, 1);
+    }
+
+    window_order.push(id);
+    
+    return window_order.length() - 1;
+  };
+
+  const createApp = (value, index) => {
+    return <Window managerRef={rndManagerRef} color={apps[index].color} />
+  };
+
+  const createApps = () => {
+    return apps.map(createApp)
+  };
+
+  return(
+    <div className="App">
+      <h1> Hello, World! </h1>
+      {createApps()}
+    </div>
+  );
 }
 
 export default App;
--- src/Window.js
+++ src/Window.js
@@ -4,13 +4,30 @@ import "./App.css";
 
 class Window extends Component {
 
-    onClick() {
-
-    }
-
     render(){
         return(
-            <Rnd style={{backgroundColor: this.props.color}} className="window" default={{x: 0, y: 0, width: 320, height: 200}} dragHandleClassName="window-handle">
+            <Rnd
+                style={{backgroundColor: this.props.color}}
+                className="window"
+                default={{x: 0, y: 0, width: 320, height: 200}}
+                dragHandleClassName="window-handle"
+                onDragStart={(e, node) => {
+                    const manager = this.props.managerRef.current;
+                    /*if (manager.prevDraggedNode) {
+                        manager.prevDraggedNode.style.zIndex = manager.prevDraggedNodeZIndex;
+                    }
+
+                    manager.prevDraggedNode = node.node;
+                    manager.prevDraggedNodeZIndex = manager.prevDraggedNode.style.zIndex;
+                    manager.prevDraggedNode.style.zIndex = manager.maxZIndex;*/
+                    
+                    
+                    manager.maxZIndex += 1
+                    manager.prevDraggedNode = node.node
+                    manager.prevDraggedNode.style.zIndex = manager.maxZIndex
+
+                }}
+            >
                 <div className="window-handle"></div>
                 <h1> Hello, World! </h1>
                 <h2>Test</h2>
